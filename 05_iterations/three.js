59dd482701e40dc7173632e0e8ea30ce9ed0c479
ashesh gupta <guptaashesh180@gmail.com>
2025-09-27 19:12:19 +0530

    js txt file

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -1,2 +1,30 @@
 //for of loop
 
+
+//for of loop
+//--------this loops are more array specifics.
+// ["","",""]
+// [{},{},{}]
+
+const arrays = [1,2,3,4,5]
+
+for (const elem of arrays) {
+    console.log(elem);
+}
+
+// same will work for the string also. 
+
+//for of loop
+//--------this loops are more array specifics.
+// ["","",""]
+// [{},{},{}]
+
+let str = "ASHESH"
+
+for (const elem of str) {
+    console.log(elem);
+}
+
+//Maps
+
+const map = new Map();
